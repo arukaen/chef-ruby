15e0e445a405283d04318d7a52678db7744be166
arukaen <c@cristhekid.com>
2015-07-21 18:07:32 -0400

    package recipe chefspec tests

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -1,2 +1,22 @@
 require 'spec_helper'
 
+describe 'ruby_basic::package' do
+    let(:platform) { 'ubuntu' }
+    let(:platform_version) { '14.04' }
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: platform, version: platform_version).converge(described_recipe) }
+
+    it 'install ruby packages' do
+        expect(chef_run).to install_package('ruby-full')
+        expect(chef_run).to install_package('ruby-dev')
+    end
+
+    context 'rhel system' do
+        let(:platform) { 'centos' }
+        let(:platform_version) { '7.0' }
+
+    it 'should install ruby packages' do
+        expect(chef_run).to install_package('ruby')
+        expect(chef_run).to install_package('ruby-devel')
+    end
+    end
+end
